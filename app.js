9449824ccc8fb96591d90367bdcca0261a79175a
oondels <hendriusfelix@gmail.com>
2024-06-01 08:10:30 -0300

    start project

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.listen(3000);
-
-app.get("/", (req, res) => {
-  res.send("<p>Manual de Máquinas!</p>");
-});
---
+++ manual/manualMaq/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ManualmaqConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'manualMaq'
