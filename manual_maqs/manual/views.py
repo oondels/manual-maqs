64fe06aa696df1b061139dd19e756bdf4ace57ec
oondels <hendriusfelix@gmail.com>
2024-06-07 16:33:08 -0300

    json database

--- manual_maqs/manual/views.py
+++ manual_maqs/manual/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import loader
 from django.http import HttpResponse
+import json
 
 def home(request):
+    with open("../maqs_DB/manual_maqs_noSQL.json", "r", encoding="UTF-8") as manual_maquinas:
+        manual_maquinas_db = json.load(manual_maquinas)
+    
+    context = {
+        "manual_maquinas_db": manual_maquinas_db,
+    }
     template = loader.get_template("home.html")
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context, request))
