aded6464b354d9b7a0cb5c906af5092595d52987
oondels <hendriusfelix@gmail.com>
2024-06-10 16:59:35 -0300

    .

--- manual_maqs/manual/views.py
+++ manual_maqs/manual/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import loader
 from django.http import HttpResponse
 import json
+import pymongo
 
 def home(request):
-    with open("../maqs_DB/manual_maqs_noSQL.json", "r", encoding="UTF-8") as manual_maquinas:
-        manual_maquinas_db = json.load(manual_maquinas)
+    myclient = pymongo.MongoClient("mongodb://localhost:27017")
+    myDb = myclient["local"]
+    manual_maquinas_db = myDb["manual maq"]
     
     context = {
-        "manual_maquinas_db": manual_maquinas_db,
+        "manual_maquinas_db": manual_maquinas_db.find(),
     }
     template = loader.get_template("home.html")
     return HttpResponse(template.render(context, request))
--- maqs_DB/teste.py
+++ maqs_DB/teste.py
@@ -1,4 +1,5 @@
 import json
+import pymongo
 
 with open("manual_maqs_noSQL.json", "r", encoding="utf-8") as manual_maquinas_file:
     manual_maquinas = json.load(manual_maquinas_file)
@@ -6,7 +7,10 @@ with open("manual_maqs_noSQL.json", "r", encoding="utf-8") as manual_maquinas_fi
     for i in manual_maquinas:
         for j in manual_maquinas[i]:
             count += 1
-            
-    print(count)
 
-
+myclient = pymongo.MongoClient("mongodb://localhost:27017")
+myDb = myclient["local"]
+manual_maq = myDb["manual maq"]
+print(type(manual_maq))
+for i in manual_maq.find():
+    print(i)
